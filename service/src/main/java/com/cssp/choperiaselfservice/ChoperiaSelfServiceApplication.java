b42baafcef7a56390a8dbe6f09f01ec4a7fd2a39
laylac19 <laylamscouto@gmail.com>
2023-03-15 11:34:59 -0300

    criação do front e do back com adição do coker

---
+++ service/src/main/java/com/cssp/choperiaselfservice/ChoperiaSelfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cssp.choperiaselfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoperiaSelfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoperiaSelfServiceApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/cssp/choperiaselfservice/ChoperiaSelfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssp.choperiaselfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoperiaSelfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
