bf5e6024106ed12e7c51a2ae278f81d6e635886d
Wagner Bozzetti Batista <wagner.batista@basis.com.br>
2023-03-25 18:51:46 -0300

    estrutura front com componentes gerados

---
+++ cssp-web/src/app/modules/chope/chope-view/chope-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chope-view',
+  templateUrl: './chope-view.component.html',
+  styleUrls: ['./chope-view.component.scss']
+})
+export class ChopeViewComponent {
+
+}
