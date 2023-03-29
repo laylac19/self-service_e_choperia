8fe61917cfb00e8a468fc511e8089faef6eed6d9
laylac19 <laylamscouto@gmail.com>
2023-03-29 15:42:43 -0300

    alterações e separação de métodos por reponsabilidade de entidade

--- service/src/main/java/com/cssp/choperiaselfservice/controller/PerfilController.java
+++ service/src/main/java/com/cssp/choperiaselfservice/controller/PerfilController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/usuario")
+@RequestMapping("api/perfil")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
 public class PerfilController {
