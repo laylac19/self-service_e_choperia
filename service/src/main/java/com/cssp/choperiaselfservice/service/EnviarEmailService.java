a4a399ee8aca3f11064dd0c041577f9209ed3196
laylac19 <laylamscouto@gmail.com>
2023-05-09 07:45:44 -0300

    enviando email pra cliente

--- service/src/main/java/com/cssp/choperiaselfservice/service/EnviarEmailService.java
+++ service/src/main/java/com/cssp/choperiaselfservice/service/EnviarEmailService.java
@@ -1,6 +1,5 @@
 package com.cssp.choperiaselfservice.service;
 
-import com.cssp.choperiaselfservice.domain.enums.DiretoriaExecutiva;
 import com.cssp.choperiaselfservice.service.dto.BuildEmailPurchadeDTO;
 import com.cssp.choperiaselfservice.service.dto.EmailClienteCompraDTO;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +18,7 @@ public class EnviarEmailService {
         log.info("Enviando Email");
         SimpleMailMessage message = new SimpleMailMessage();
         BuildEmailPurchadeDTO build = emailDTO.getBuild();
-        message.setTo(DiretoriaExecutiva.findEmails());
+        message.setTo(build.getEmailCliente());
         message.setSubject("CSSP Compras");
         message.setText("Olá " + build.getNomeCliente() + "," +
                 "\n\n" + emailDTO.getMensagem() +
@@ -28,18 +27,4 @@ public class EnviarEmailService {
         envioEmailJava.send(message);
         log.info("Enviado");
     }
-
-//    public void purchasesMade(EmailClienteCompraDTO emailDTO) {
-//        log.info("Enviando Email");
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        BuildEmailPurchadeDTO build = emailDTO.getBuild();
-//        message.setTo(build.getEmailCliente());
-//        message.setSubject("CSSP Compras");
-//        message.setText("Olá " + build.getNomeCliente() + "," +
-//                "\n\n" + emailDTO.getMensagem() +
-//                "\n\n Data Compra: " + build.getDataCompra() + " - Valor Compra: " + build.getValorCompra() +
-//                "\n\n\n\n Obrigada pela prefeência!!!");
-//        envioEmailJava.send(message);
-//        log.info("Enviado");
-//    }
 }
