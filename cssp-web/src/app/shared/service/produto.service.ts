84b285837d6f01ffb8e6869e7694d6a5541da766
layla.couto <laylamscouto@gmail.com>
2023-08-22 17:50:53 -0300

    ajustes finais

--- cssp-web/src/app/shared/service/produto.service.ts
+++ cssp-web/src/app/shared/service/produto.service.ts
@@ -52,7 +52,7 @@ export class ProdutoService {
   }
 
   getWeightValue(): Observable<any> {
-    return this.http.get("http://192.168.0.103/peso");
+    return this.http.get("http://192.168.1.150/peso");
   }
 
   balanceReportProductInStock(): Observable<ProdutoEstoqueReportModel[]> {
